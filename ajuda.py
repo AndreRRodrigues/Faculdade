a00a1d665040bf0b4d35146538721ce16dbd2365
Andre <ribeiro.andredev@outlook.com>
2022-04-06 13:41:39 -0300

    first commit

---
+++ ajuda.py
@@ -0,0 +1,9 @@
+m1=int(input('digite um numero: '))
+m2=int(input('digite um numero: '))
+m3=int(input('digite um numero: '))
+
+mp=(m1*2)+(m2*3)+(m3*5)
+print(mp)
+md=mp/(2+3+5)
+
+print('a media ponderada é {}'.format(md))
---
+++ introdução1.py
@@ -0,0 +1,54 @@
+
+m1=23
+m2=32
+m3=64
+media=m1+m2+m3/3
+print("a media das contas m1:{}, m2:{}, m3:{} é {}".format(m1, m2, m3, media))
+'''
+ll1=9*7
+ll2=5*7
+lp=6
+lad=ll1+ll2
+com=6*17
+print("1: Serão necessario {} ladrilhos".format(lad))
+print("2: Nos 17 sacos ao todo terão {} ladrilhos sobrão {} ladrilhos".format(com, (com-lad)))
+
+x = 15
+y = 4
+
+print ('x + y = ', x+y)
+
+print ('x - y = ', x-y)
+
+print ('x * y = ', x*y)
+
+print ('x/y = ', x/y)
+
+print ('x // y = ', x//y)
+
+print ('x ** y = ', x**y)
+
+x = 10
+y = 12
+
+print ('x > y é ', x>y)
+
+print ('x < y é ', x<y)
+
+print ('x == y é ', x==y)
+
+print ('x != y é ',x!=y)
+
+print ('x >= y é ', x>=y)
+
+print ('x <= y é ', x<=y)
+
+x = True
+y = False
+
+print ('x and y é ', x and y)
+
+print ('x or y é ', x or y)
+
+print ('not x é ', not x)
+''' 
