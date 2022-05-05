a387bbf91440e1bd0acbe55277f3c59ee46aaca0
Andre Ribeiro <ribeiro.andredev@outlook.com>
2022-05-05 20:05:27 -0300

    Dia 05/05

--- exercicios/unidade 2/exercicio 2.py
+++ exercicios/unidade 2/exercicio 2.py
@@ -4,9 +4,11 @@ media = float(input("Qual a media do aluno: "))
 m = 6.0
 al = 200
 
-if faltas < (al*0.75):
-    print("O aluno foi REPROVADO")
-elif media < m or faltas < (al*0.75):
+if faltas > (al*0.75):
     print("O aluno foi REPROVADO")
 
+elif media < m or faltas > (al*0.75):
+    print("O aluno foi REPROVADO")
 
+elif faltas < (al*0.75):
+    print("O aluno foi APROVADO <3")
